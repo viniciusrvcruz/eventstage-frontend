5627c03ec21afb9726b008c565c248a22a37f001
viniciusrvcruz <viniciusrobertopro@gmail.com>
2025-02-20 00:05:06 -0300

    feat: first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import { ArrowRight, Copy } from 'lucide-react'
+import { Button } from "@/components/button";
+import { IconButton } from '@/components/icon-button';
+
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <Button type="submit">
+        Enviar
+        <ArrowRight />
+      </Button>
+      <IconButton>
+        <Copy />
+      </IconButton>
     </main>
   );
 }
