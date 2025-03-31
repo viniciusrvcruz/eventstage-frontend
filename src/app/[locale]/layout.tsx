a74d736acdc5fd56edc8654ab033fe48f6e27d07
viniciusrvcruz <viniciusrobertopro@gmail.com>
2025-03-31 18:55:21 -0300

    feat: next-intl integration

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,9 +1,12 @@
 import './globals.css'
 
 import logo from '@/assets/logo.svg'
+import { routing } from '@/i18n/routing'
 import type { Metadata } from 'next'
+import { NextIntlClientProvider, hasLocale } from 'next-intl'
 import { Montserrat, Oxanium } from 'next/font/google'
 import Image from 'next/image'
+import { notFound } from 'next/navigation'
 import { ToastContainer } from 'react-toastify'
 
 export const metadata: Metadata = {
@@ -22,13 +25,23 @@ const montserrat = Montserrat({
   variable: '--font-montserrat',
 })
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
+  params,
 }: Readonly<{
   children: React.ReactNode
+  params: Promise<{ locale: string }>
 }>) {
+  const { locale } = await params
+  if (!hasLocale(routing.locales, locale)) {
+    notFound()
+  }
+
   return (
-    <html lang="pt-BR" className={`${oxanium.variable} ${montserrat.variable}`}>
+    <html
+      lang={locale}
+      className={`${oxanium.variable} ${montserrat.variable}`}
+    >
       <body className="flex items-center flex-col bg-gray-900 text-gray-100 antialiased bg-[url(/background.png)] bg-no-repeat md:bg-right-top">
         <header className="flex justify-start max-w-[1240px] w-full py-5 px-5 md:p-8">
           <Image src={logo} alt="devstage" width={108.5} height={30} />
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,10 @@
+import createMiddleware from 'next-intl/middleware'
 import {
   type MiddlewareConfig,
   type NextRequest,
   NextResponse,
 } from 'next/server'
+import { routing } from './i18n/routing'
 
 const publicRoutes = [
   { path: '/login', whenAuthenticated: 'redirect' },
@@ -12,13 +14,15 @@ const publicRoutes = [
 
 const REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE = '/login'
 
+const intlMiddleware = createMiddleware(routing)
+
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname
-  const publicRoute = publicRoutes.find((route) => route.path === path)
+  const publicRoute = publicRoutes.find((route) => path.includes(route.path))
   const authToken = request.cookies.get('token')
 
   if (!authToken && publicRoute) {
-    return NextResponse.next()
+    return intlMiddleware(request)
   }
 
   if (!authToken && !publicRoute) {
@@ -29,11 +33,7 @@ export function middleware(request: NextRequest) {
     return NextResponse.redirect(redirectUrl)
   }
 
-  if (
-    authToken &&
-    publicRoute &&
-    publicRoute.whenAuthenticated === 'redirect'
-  ) {
+  if (authToken && publicRoute?.whenAuthenticated === 'redirect') {
     const redirectUrl = request.nextUrl.clone()
 
     redirectUrl.pathname = '/home'
@@ -41,11 +41,11 @@ export function middleware(request: NextRequest) {
     return NextResponse.redirect(redirectUrl)
   }
 
-  return NextResponse.next()
+  return intlMiddleware(request)
 }
 
 export const config: MiddlewareConfig = {
   matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|background.png).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|background.png|trpc|_next|_vercel).*)',
   ],
 }
